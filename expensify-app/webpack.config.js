4b426a291ebc74c860232c663df051d9900b9f18
pb25193 <pb25193@gmail.com>
2019-06-16 19:46:30 +0530

    set up production webpack build :'(

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,31 +1,47 @@
 // entry --> output
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+module.exports = (env) => {
+    const isProduction = env === 'production';
 
-module.exports = {
-    entry: "./src/app.js",
-    output: { 
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-}
+    return {
+        entry: "./src/app.js",
+        output: { 
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+                test: /\.s?css$/
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({ filename: "styles.css" })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+};
